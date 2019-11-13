dd786549f91935a77a54cf850d6ed449204a9dae
Jonathan Byrne <jonathanpatrickbyrne@gmail.com>
2019-11-13 18:51:07 +0000

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<style>
+	main {
+		max-width: 640px;
+	}
+
+	h1 {
+		background: linear-gradient(to bottom right, #3BB2B8 0%, #42E695 100%);
+		-webkit-background-clip: text;
+				background-clip: text;
+		-webkit-text-fill-color: transparent;
+		color: #3BB2B8;
+	}
+
+	h2 {
+		margin: 21px 0;
+	}
+
+	p {
+		margin-bottom: 9px;
+	}
+
+	p:last-child {
+		margin-bottom: 0;
+	}
+
+	@media(max-width: 500px) {
+		h2 {
+			margin: 16px 0;
+		}
+		
+		p {
+			margin-bottom: 7px;
+		}
+	}
+</style>
+
+<main>
+    <h1>Hello.</h1>
+    <h2>My name is Jonathan Byrne.</h2>
+    <p>I’m a UI Engineer at Accenture The Dock, based in Dublin.</p>
+    <p>Previously I worked at Fjord, IBM Watson Health and IADT.</p>
+    <p>You can get in touch with me at <a href="mailto:hello@jonathanbyrne.net">hello@jonathanbyrne.net.</a></p>
+</main>
