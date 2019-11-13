34ea392a49690f8c70d54ad6bde3ba542c377353
Jonathan Byrne <jonathanpatrickbyrne@gmail.com>
2019-11-13 19:45:57 +0000

    began icon component implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Icon from './components/Icon.svelte';
+</script>
+
 <style>
 	main {
 		max-width: 640px;
@@ -22,12 +26,39 @@
 	p:last-child {
 		margin-bottom: 0;
 	}
+
+	ul {
+		display: flex;
+		flex-direction: row;
+		padding: 0;
+		list-style: none;
+	}
 </style>
 
 <main>
     <h1>Hello.</h1>
+
     <h2>My name is Jonathan Byrne.</h2>
+
     <p>I’m a UI Engineer at Accenture The Dock, based in Dublin.</p>
     <p>Previously I worked at Fjord, IBM Watson Health and IADT.</p>
     <p>You can get in touch with me at <a href="mailto:hello@jonathanbyrne.net">hello@jonathanbyrne.net</a>.</p>
+
+	<!-- <ul>
+		<li>
+			<a href="https://www.linkedin.com/in/jonathanpbyrne" target="_blank">
+				<Icon symbol="linkedin" />
+			</a>
+		</li>
+		<li>
+			<a href="https://github.com/jpbyrne" target="_blank">
+				<Icon symbol="github" />
+			</a>
+		</li>
+		<li>
+			<a href="https://twitter.com/_jpbyrne" target="_blank">
+				<Icon symbol="twitter" />
+			</a>
+		</li>
+	</ul> -->
 </main>
