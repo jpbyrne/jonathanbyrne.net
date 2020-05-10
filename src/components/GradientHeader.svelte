1b74b33216fbbb8316474115387324d680b7346a
Jonathan Byrne <jonathanpatrickbyrne@gmail.com>
2020-05-10 09:41:18 +0100

    adds animation to gradient header

---
+++ src/components/GradientHeader.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let gradient;
+</script>
+
+<style>
+	@keyframes flow {
+		0% {
+			background-position-x: 0;
+		}
+		50% {
+			background-position-x: 100%;
+		}
+		100% {
+			background-position-x: 0;
+		}
+  }
+
+	h1 {
+		animation: flow 7s ease-in-out infinite;
+		background: linear-gradient(to right, var(--left-color), var(--right-color));
+		background-size: 200%;
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+		color: var(--gradient-left-color-1);
+	}
+</style>
+
+<h1 style="--left-color: {gradient.left};--right-color: {gradient.right};">
+  <slot />
+</h1>
